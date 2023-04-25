87145614bd8f894afe9462538412c41aa367a7c9
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2023-04-25 18:40:38 +0200

    fix logger

--- mito_utils/utils/helpers.py
+++ mito_utils/utils/helpers.py
@@ -75,12 +75,12 @@ def make_folder(path, name, overwrite=True):
 ##
 
 
-def set_logger(path_runs, name, mode='w'):
+def set_logger(path, name, mode='w'):
     """
     A function to open a logs.txt file for a certain script, writing its trace at path_main/runs/step/.
     """
     logger = logging.getLogger("mito_benchmark")
-    handler = logging.FileHandler(path_runs + name, mode=mode)
+    handler = logging.FileHandler(os.path.join(path, name), mode=mode)
     handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
