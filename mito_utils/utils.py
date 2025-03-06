599f59d2f0351508694ad6f6ab0300a3c1212554
Andrea Cossa <andrea.cossa05@gmail.com>
2025-03-06 12:22:21 +0100

    add extract_kwargs

--- mito_utils/utils.py
+++ mito_utils/utils.py
@@ -166,7 +166,7 @@ def format_tuning(path_tuning):
     """
     Format tuning dataframe.
     """
-    
+
     assert os.path.exists(path_tuning)
     options = pd.read_csv(os.path.join(path_tuning, 'all_options_final.csv'))
     metrics = pd.read_csv(os.path.join(path_tuning, 'all_metrics_final.csv'))
@@ -179,6 +179,117 @@ def format_tuning(path_tuning):
     metrics = metrics['metric'].unique().tolist()
 
     return df, metrics, options
+
+
+##
+
+
+def extract_kwargs(args, only_tree=False):
+    """
+    Extract preprocessing parameters from CLI and tuning information. 
+    """
+    
+    path_tuning = args.path_tuning if hasattr(args, 'path_tuning') else None
+
+    if path_tuning is not None and args.job_id is not None:
+        
+        path_options = os.path.join(path_tuning, 'all_options_final.csv')
+        if os.path.exists(path_options):
+            
+            df_options = pd.read_csv(path_options).loc[lambda x: x['job_id'] == args.job_id]
+            d = { k:v for k,v in zip(df_options['option'],df_options['value']) }
+
+            if not only_tree:
+
+                cell_filter = d['cell_filter']
+                min_cell_number = int(d['min_cell_number'])
+                lineage_column = d['lineage_column']
+                filtering = d['filtering']
+                bin_method = d['bin_method']
+                min_n_var = int(d['min_n_var'])
+                kwargs = {
+                    'min_cell_number' : min_cell_number,
+                    'lineage_column' : lineage_column,
+                    'filtering' : filtering,
+                    'bin_method' : bin_method,
+                    'min_n_var' : min_n_var,
+                    'path_dbSNP' : args.path_dbSNP, 
+                    'path_REDIdb' : args.path_REDIdb,
+                    'ncores' : args.ncores
+                }
+                filtering_kwargs = {
+                    'min_cov' : int(d['min_cov']),
+                    'min_var_quality': int(d['min_var_quality']),
+                    'min_frac_negative' : float(d['min_frac_negative']),
+                    'min_n_positive' : int(d['min_n_positive']),
+                    'af_confident_detection' : float(d['af_confident_detection']),
+                    'min_n_confidently_detected' : int(d['min_n_confidently_detected']),
+                    'min_mean_AD_in_positives' : float(d['min_mean_AD_in_positives']),
+                    'min_mean_DP_in_positives' : float(d['min_mean_DP_in_positives']) 
+                }
+                binarization_kwargs = {
+                    't_prob' : float(d['t_prob']), 
+                    't_vanilla' : float(d['t_vanilla']),
+                    'min_AD' : int(d['min_AD']),
+                    'min_cell_prevalence' : float(d['min_cell_prevalence']),
+                    'k' : int(d['k']), 
+                    'gamma' :  float(d['gamma']), 
+                    'resample' : False
+                }
+                tree_kwargs = {'solver':d['solver'], 'metric':d['metric']}
+            
+            else:
+
+                cell_filter = None; kwargs = None; 
+                filtering_kwargs = None; binarization_kwargs = None
+                tree_kwargs = {'solver':d['solver'], 'metric':d['metric']}
+
+        else:
+            raise ValueError(f'{path_options} does not exists!')
+
+    else:
+
+        if not only_tree:
+
+            cell_filter = args.cell_filter
+            kwargs = {
+                'min_cell_number' : args.min_cell_number,
+                'lineage_column' : args.lineage_column,
+                'filtering' : args.filtering,
+                'bin_method' : args.bin_method,
+                'min_n_var' : args.min_n_var,
+                'path_dbSNP' : args.path_dbSNP, 
+                'path_REDIdb' : args.path_REDIdb,
+                'ncores' : args.ncores
+            }
+            filtering_kwargs = {
+                'min_cov' : args.min_cov,
+                'min_var_quality': args.min_var_quality,
+                'min_frac_negative' : args.min_frac_negative,
+                'min_n_positive' : args.min_n_positive,
+                'af_confident_detection' : args.af_confident_detection,
+                'min_n_confidently_detected' : args.min_n_confidently_detected,
+                'min_mean_AD_in_positives' : args.min_mean_AD_in_positives,
+                'min_mean_DP_in_positives' : args.min_mean_DP_in_positives 
+            }
+            binarization_kwargs = {
+                't_prob' : args.t_prob, 
+                't_vanilla' : args.t_vanilla,
+                'min_AD' : args.min_AD,
+                'min_cell_prevalence' : args.min_cell_prevalence,
+                'k' : args.k, 
+                'gamma' : args.gamma, 
+                'resample' : False
+            }
+            tree_kwargs = {'solver':args.solver, 'metric':args.metric}
+        
+        else:
+
+            cell_filter = None; kwargs = None; 
+            filtering_kwargs = None; binarization_kwargs = None
+            tree_kwargs = {'solver':args.solver, 'metric':args.metric}
+
+    return cell_filter, kwargs, filtering_kwargs, binarization_kwargs, tree_kwargs
 
 
 ##
